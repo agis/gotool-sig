e044d2f94a0e9460df71602acd4e4470f9c692b2
Agis Anastasopoulos <agis@clerk.dev>
2025-08-26 15:34:09 +0300

    foo

--- main.go
+++ main.go
@@ -4,13 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
+	"syscall"
 )
 
 func main() {
-	ch := make(chan os.Signal, 100)
-	signal.Notify(ch)
+	if len(os.Args) != 2 {
+		panic("Usage: program <buffer_size>")
+	}
 
-	fmt.Println("Signal monitor started. Waiting for signals...")
+	bufferSize, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(fmt.Sprintf("Invalid buffer size: %s must be an integer", os.Args[1]))
+	}
+	if bufferSize <= 0 {
+		panic("Buffer size must be greater than 0")
+	}
+
+	ch := make(chan os.Signal, bufferSize)
+	signal.Notify(ch, syscall.SIGUSR1)
+
+	fmt.Printf("Signal monitor started with buffer size %d. Waiting for SIGUSR1...\n", bufferSize)
 
 	for sig := range ch {
 		fmt.Printf("Received signal: %s (%d)\n", sig.String(), sig)
